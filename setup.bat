0b076fd52e4fdf148b5ad860939ec68273ecb27c
danllaq <128614450+danllaq@users.noreply.github.com>
2023-09-05 23:22:33 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,9 +3,15 @@ title PyroDAQ
 
 rem Creating virtual environment
 python -m venv PyroDAQvenv
+echo Creating the virtual environment...
 
 rem Activating the virtual environment
 PyroDAQvenv\Scripts\activate
+echo Activating the virtual environment...
 
 rem Installing dependencies
 pip install -r requirements.txt
+echo Installing the required dependencies...
+
+rem Display message to confirm that the script has completed
+echo Setup completed!
